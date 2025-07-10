7a511b97e30d95dd6934d6a6dd91b8c46c25d6eb
hyeonseo1109 <alsgustj2@naver.com>
2025-07-10 14:16:53 +0900

    기존의 scss 스타일을 styled-components 스타일로 변환함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from 'styled-components';
+import { flexMixin } from "./export";
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: white;
+  }
+
+  body {
+    background-color: #121212;
+  }
+`;
+
+
+const Section = styled.section `
+  ${flexMixin(undefined, 'center', 'center', 'wrap', '20px')};
+  padding: 20px 40px;
+`
+
+
+
 
 function App() {
   return (
     <main>
+      <GlobalStyle />
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,30 @@
+import styled from 'styled-components';
+import { flexMixin, fontMixin } from '../export';
+
+const HeaderContainer = styled.header `
+  ${flexMixin (undefined, 'space-between', 'center')};
+  padding: 20px 20px;
+  background-color: black;
+
+  ul {
+    ${flexMixin (undefined, 'center', 'center', '20px')};
+
+    li {
+      ${fontMixin('16px')}
+      list-style: none;
+    }
+  }
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <HeaderContainer>
       <h1>OZ코딩스쿨</h1>
       <ul className="menu-container">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
       </ul>
-    </header>
+    </HeaderContainer>
   );
 }
